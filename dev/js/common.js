99df5317452207767090aa301686eb1df703f090
jakhongir-i <jahangirinatov@gmail.com>
2020-06-05 22:54:50 +0500

    initial commit

---
+++ dev/js/common.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+
+
+
+
+  /*
+  * Example page script
+  */
+  var pagesCount = document.querySelectorAll('.ex-pages__list-link').length;
+  document.querySelector('.ex-pages__count').innerHTML = `${pagesCount}`;
+
+
+};
+
